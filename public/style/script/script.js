135b2f3a746944396bca4d5e4d3836222c440b1c
JasinAhmed <jaasin9@gmail.com>
2026-03-29 14:29:27 +0200

    script heet nu client.js

--- public/style/script/script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('hoofd-menu');
-
-hamburger.addEventListener('click', () => {
-  menu.classList.toggle('open');
-  hamburger.textContent = menu.classList.contains('open') ? '✕' : '☰';
-});
