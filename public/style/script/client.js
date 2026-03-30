2f19375718ecb7a678e25e93baa4ec5182235417
JasinAhmed <jaasin9@gmail.com>
2026-03-30 19:28:51 +0200

    #25 detecteer JavaScript en voeg .js class toe aan html element

--- public/style/script/client.js
+++ public/style/script/client.js
@@ -1,3 +1,7 @@
+/* Ik geef aan dat JavaScript actief is */
+document.documentElement.classList.add('js');
+
+/* Ik pak de hamburger en het menu */
 const hamburger = document.getElementById('hamburger');
 const menu = document.getElementById('hoofd-menu');
 
